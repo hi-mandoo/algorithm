6de1c98cabc9bf7b2fc7d1daea51e7fed7a2487b
himandoo <himandoooo@gmail.com>
2025-11-20 12:43:26 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -7,7 +7,7 @@ int main() {
     
     for (int i = 1; i <= testCase; i++) {
         int a, b;
-        cin >> a >> b;
-        cout << "Case #" << i << ": " << a + b << "\n";
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a+b);
     }
 }
