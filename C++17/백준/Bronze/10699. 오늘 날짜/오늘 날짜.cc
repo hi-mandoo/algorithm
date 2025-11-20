fc69cc24cde3aa14cdefc3a3458bc1af3d73b9b1
himandoo <himandoooo@gmail.com>
2025-11-20 12:50:20 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    time_t timer = time(NULL);
+    struct tm* t = localtime(&timer);
+    printf("%d-%d-%d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+}
