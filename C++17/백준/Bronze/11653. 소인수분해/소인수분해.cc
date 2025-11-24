8c640dbc7ee9440cf612139f40b763f2ea54d416
himandoo <himandoooo@gmail.com>
2025-11-24 21:13:12 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ C++17/백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -5,13 +5,19 @@ int main() {
     int n;
     cin >> n;
     
-    int prime = 2;
-    while(n > 1) {
-        if (n % prime == 0) {
-            n /= prime;
-            printf("%d\n", prime);
-        } else {
-            prime++;
+    while(n%2==0) {
+        n /= 2;
+        printf("%d\n", 2);
+    }
+    
+    for(int i = 3; i*i<=n; i+=2) {
+        while (n % i == 0) {
+            printf("%d\n", i);
+            n /= i;
         }
+    }
+    
+    if (n > 1) {
+        printf("%d\n", n);
     }
 }
