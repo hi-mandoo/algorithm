73346cd414b25aae70f1d5336d41cc97d5e08a80
himandoo <himandoooo@gmail.com>
2025-11-26 23:55:47 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    if (a > b) {
+        cout << ">";
+    } else if (b > a) {
+        cout << "<";
+    } else {
+        cout << "==";
+    }
+    
+    
+}
