592d8ceede96d238b44fae0bca8218459826dd10
himandoo <himandoooo@gmail.com>
2025-11-30 18:56:14 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ C++17/백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -2,21 +2,11 @@
 using namespace std;
 
 int main() {
+    char output[] = {'D', 'C', 'B', 'A', 'E'};
+    
     for (int i = 0; i < 3; i++) {
         int a, b, c, d;
         scanf("%d %d %d %d", &a, &b, &c, &d);
-        int result = a + b + c + d;
-        
-        if (result == 3) {
-            printf("%c\n", 'A');
-        } if (result == 2) {
-            printf("%c\n", 'B');
-        } if (result == 1) {
-            printf("%c\n", 'C');
-        } if (result == 0) {
-            printf("%c\n", 'D');
-        } if (result == 4) {
-            printf("%c\n", 'E');
-        }
+        printf("%c\n", output[a + b + c + d]);
     }
 }
