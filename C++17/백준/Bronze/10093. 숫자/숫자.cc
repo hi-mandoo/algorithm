7968edec23c2f9cace68ea0b6eba5310d0fc589a
himandoo <himandoooo@gmail.com>
2025-12-01 20:38:23 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/10093. 숫자/숫자.cc
+++ C++17/백준/Bronze/10093. 숫자/숫자.cc
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <utility>
 using namespace std;
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long a, b;
+    cin >> a >> b;
     
     if (a > b) {
         swap(a, b);
@@ -13,9 +15,9 @@ int main() {
     if (a == b) {
         cout << 0;
     } else {
-        printf("%d\n", b - a - 1);
-        for (int i = a + 1; i< b; i++) {
-            printf("%d ", i);
+        cout << b - a - 1 << "\n";
+        for (long long i = a + 1; i< b; i++) {
+            cout << i << " ";
         }
     }
 }
